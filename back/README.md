10232f82e6cb3ac35fca892f92543c8f87e0827d
n.x <zkolsh@proton.me>
2026-04-21 09:16:59 -0300

    basic backend with no db

--- back/README.md
+++ back/README.md
@@ -1,1 +1,18 @@
-correr el server:  `node src/index.js`
+Bootstrapping del servidor
+==========================
+
+Crear el archivo `back/.env` con:
+```
+SERVER_PORT=55500
+JWT_SECRET=AlejandroGuillermoSchujman
+FRONTEND_URL=http://localhost:5173/
+```
+
+Luego:  `npm install`
+
+Correr el servidor
+==================
+
+```
+$ npm run dev
+```
--- back/src/index.js
+++ back/src/index.js
@@ -1,1 +1,48 @@
-console.log("PINGA")
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+var jwt = require('jsonwebtoken')
+const app = express()
+const port = process.env.SERVER_PORT
+
+app.use(express.json())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
+
+console.log("#####")
+
+app.get('/censurado', (req, res) => {
+    console.log("Se ha obtenido la #####")
+    res.send("#####");
+})
+
+app.post('/login', (req, res) => {
+    if (!req.body || !req.body.username || !req.body.password) {
+        res.status = 400;
+        res.send("Bad request");
+        return;
+    };
+
+    const userId = Math.random() * 100000000;
+    const pwd = Math.random() > 0.2;
+    if (!pwd) {
+        console.log('Autenticación rechazada para ' + req.body.username + " (" + req.body.password + ")");
+        res.status = 403;
+        res.send('Usuario o contraseña incorrectos.');
+        return;
+    };
+
+    var token = jwt.sign({
+        userID: userId
+    }, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+    });
+
+    res.json({token});
+})
+
+app.listen(port, () => {
+    console.log("Recibiendo #####s")
+})
